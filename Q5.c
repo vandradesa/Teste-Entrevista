11b56e33f210f42bca206c1f40180e32aaff29df
Vanessa Andrade <v.andradd@gmail.com>
2024-02-21 15:09:03 -0300

    codigos com enunciados das questoes

--- Q5.c
+++ Q5.c
@@ -1,3 +1,9 @@
+/* //5) Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+b) Evite usar funções prontas, como, por exemplo, reverse; */
+
 #include <stdio.h>
 #include <stdlib.h>
 
